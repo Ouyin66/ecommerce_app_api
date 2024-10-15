db9293f09260388a915d52fa16fb34b800b719d8
dunno.wut2do <92363678+Ouyin66@users.noreply.github.com>
2024-10-15 18:36:02 +0700

    create widget login

---
+++ lib/config/const.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// COLOR
+const branchColor = Color(0xFFED1B24);
+const whiteColor = Color(0xFFFFFFFF);
+const blackColor = Color(0xFF1D1D1D);
+const greyColor = Color(0xFF959595);
+
+// IMAGE
+
+const urlLogo = 'assets/images/UNIQLO.png';
+const urlLogo2 = 'assets/images/UNIQLO2.png';
+const urlLogo3 = 'assets/images/UNIQLO3.png';
+const urlGoogleLogo = 'assets/images/Google_Logo.png';
+const urlFacebookLogo = 'assets/images/Facebook_Logo.png';
+const urlForgetPassword = 'assets/images/Forget_Password.png';
+
+// FONT
+TextStyle head = GoogleFonts.barlow(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle subhead = GoogleFonts.barlow(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle body = GoogleFonts.barlow(
+  fontSize: 18,
+  color: blackColor,
+  fontWeight: FontWeight.normal,
+);
